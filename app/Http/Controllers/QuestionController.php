8f9ac4207ba01b0506a4a9ac34bd64dfa0816373
Mike Kang <mikekang@Mikes-MacBook-Pro.local>
2019-07-10 19:54:06 -0400

    lesson 19

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Model\Question;
 use Illuminate\Http\Request;
 
+use App\Http\Resources\QuestionResource;
+
 class QuestionController extends Controller
 {
     /**
@@ -14,7 +16,7 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        //
+        return Question::latest()->get();
     }
 
     /**
@@ -35,7 +37,9 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      //auth()->user()->question()->create($request->all());
+      Question::create($request->all());
+      return response('created');
     }
 
     /**
@@ -46,7 +50,7 @@ class QuestionController extends Controller
      */
     public function show(Question $question)
     {
-        //
+        return new QuestionResource($question);
     }
 
     /**
@@ -80,6 +84,7 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return response('Deleted', 200);
     }
 }
--- app/Model/Question.php
+++ app/Model/Question.php
@@ -8,6 +8,13 @@ use App\User;
 
 class Question extends Model
 {
+    protected $fillable = ['title', 'slug', 'body', 'category_id', 'user_id'];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
     public function user()
     {
       return $this->belongsTo(User::class);
@@ -21,5 +28,10 @@ class Question extends Model
     public function category()
     {
       return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute()
+    {
+      return asset("api/question/$this->slug");
     }
 }
