0ac66af38447d985d9e62af2451b53b36120a2ea
Mike Kang <mikekang@Mikes-MacBook-Pro.local>
2019-07-21 12:53:47 -0400

    Vuetify started stuff

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>Document</title>
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+  <div id="app">
+    <v-app>
+      <v-toolbar>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>Title</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat>Link One</v-btn>
+          <v-btn flat>Link Two</v-btn>
+          <v-btn flat>Link Three</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </v-app>
+  </div>
+  <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
